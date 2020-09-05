ca945ef6681680e8b949c6e56f88255ae9dc5722
Vikanshu Joshi <vikanshu2016@gmail.com>
2020-09-05 11:55:01 +0530

    completed home screen tabs

--- app/src/main/java/com/vys/todo/MainActivity.java
+++ app/src/main/java/com/vys/todo/MainActivity.java
@@ -1,14 +1,53 @@
 package com.vys.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("ToDo");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        tabLayout = findViewById(R.id.home_screen_tab_layout);
+        viewPager = findViewById(R.id.home_screen_view_pager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("UPCOMING"));
+        tabLayout.addTab(tabLayout.newTab().setText("FINISHED"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.e(TAG,tab.getText().toString());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
