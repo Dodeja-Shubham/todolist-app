5acff11dc0cb44d6c3241efcc011bd7377bb0551
Vikanshu Joshi <vikanshu2016@gmail.com>
2020-09-05 12:25:06 +0530

    completed HomeScreen view pager

--- app/src/main/java/com/vys/todo/MainActivity.java
+++ app/src/main/java/com/vys/todo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.vys.todo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TableLayout;
 
 import com.google.android.material.tabs.TabLayout;
+import com.vys.todo.Fragments.FinishedFragment;
+import com.vys.todo.Fragments.UpcomingTasksFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private TabLayout tabLayout;
-    private ViewPager2 viewPager;
+    private ViewPager viewPager;
+
+    Fragment[] fragments = {new UpcomingTasksFragment(),new FinishedFragment()};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +40,30 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.home_screen_tab_layout);
         viewPager = findViewById(R.id.home_screen_view_pager);
 
-        tabLayout.addTab(tabLayout.newTab().setText("UPCOMING"));
-        tabLayout.addTab(tabLayout.newTab().setText("FINISHED"));
+        /**adding new tabs*/
+        tabLayout.addTab(tabLayout.newTab().setText("Upcoming"));
+        tabLayout.addTab(tabLayout.newTab().setText("Finished"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        /**setting up view pager*/
+        viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                return fragments[position];
+            }
+            @Override
+            public int getCount() {
+                return fragments.length;
+            }
+        });
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        viewPager.setCurrentItem(0);
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                Log.e(TAG,tab.getText().toString());
+                viewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
